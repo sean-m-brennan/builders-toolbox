a4a054af185acd3da01482ecfb2a7abe4aa30ff4
Sean Brennan <sean.brennan@tekfive.com>
2024-06-12 13:38:00 -0600

    Multi-impl messaging - about to reduce to just domain sockets.

--- apply_copyright.py
+++ apply_copyright.py
@@ -5,16 +5,12 @@ import sys
 
 from git import Repo
 
-this_dir = os.path.dirname(os.path.realpath(__file__))
-repo = Repo(this_dir)
-git_files = repo.commit().tree.traverse()
-git_paths = [entry.abspath for entry in git_files]
 
 py_marker = '# ******************\n'
 
 
 def py_copy():
-    path = os.path.join(this_dir, 'COPY')
+    path = os.path.join(this_dir, 'COPYING')
     lines = []
     with open(path, 'r') as f:
         first = True
@@ -33,7 +29,7 @@ c_mark_end = ' *******************/\n'
 
 
 def c_copy():
-    path = os.path.join(this_dir, 'COPY')
+    path = os.path.join(this_dir, 'COPYING')
     lines = []
     with open(path, 'r') as f:
         first = True
@@ -80,8 +76,8 @@ def put_lines(lines, path, marker, idx, copytext):
         f.writelines(lines[idx:])
 
 
-def add_copy_to_source(path, dry_run, verbose):
-    if path not in git_paths:
+def add_copy_to_source(path, sources, dry_run, verbose, show_skipped):
+    if path not in sources:
         return
 
     if path.endswith(".py"):
@@ -93,7 +89,7 @@ def add_copy_to_source(path, dry_run, verbose):
             idx += 1
         put_lines(lines, path, py_marker, idx, py_copy())
 
-    elif path.endswith('.c') or path.endswith('.h'):
+    elif path.endswith('.c') or path.endswith('.cpp') or path.endswith('.h') or path.endswith('.h.in'):
         lines = get_lines(path, dry_run, verbose)
         if lines is None:
             return
@@ -109,19 +105,31 @@ def add_copy_to_source(path, dry_run, verbose):
             idx += 1
         put_lines(lines, path, c_mark_end, idx, c_copy())
 
+    # FIXME .sh, .js, .css, .tex
+    elif show_skipped:
+        print('Skipped %s' % path)
 
-def add_copy_to_sources(dry_run, verbose):
-    for root, dirs, files in os.walk(this_dir):
+
+def add_copy_to_sources(base_dir, sources, dry_run, verbose, show_skipped):
+    for root, dirs, files in os.walk(base_dir):
         for file in files:
             path = os.path.join(root, file)
-            add_copy_to_source(path, dry_run, verbose)
+            add_copy_to_source(path, sources, dry_run, verbose, show_skipped)
 
 
 if __name__ == "__main__":
+    this_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+    args = this_dir, True, False, False
     # FIXME argparse instead
+
+    repo = Repo(args[0])
+    git_files = repo.commit().tree.traverse()
+    git_paths = [entry.abspath for entry in git_files]
+
     if len(sys.argv) < 2:
-        add_copy_to_sources(dry_run=False, verbose=True)
+        add_copy_to_sources(this_dir, git_paths, dry_run=args[1], verbose=args[2], show_skipped=args[3])
     else:
         for s in sys.argv[1:]:
             filepath = os.path.join(this_dir, s)
-            add_copy_to_source(filepath, dry_run=False, verbose=True)
+            add_copy_to_source(filepath, git_paths, dry_run=args[1], verbose=args[2], show_skipped=True)
